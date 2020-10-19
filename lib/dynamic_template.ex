dc6e416a13bfc6a5080487690d70e4f66c348c41
Albert Moravec <albert.moravec@gmail.com>
2020-10-20 00:24:04 +0200

    Simplify rendering logic

--- lib/dynamic_template.ex
+++ lib/dynamic_template.ex
@@ -1,39 +1,31 @@
 defmodule DynamicTemplate do
-  def render(file_path, view_module, assigns) do
-    module_name = module_name(file_path)
-
-    if not Code.ensure_loaded?(module_name) do
-      compile(file_path, module_name, view_module)
+  def render(module_name, assigns) do
+    case Code.ensure_loaded?(module_name) do
+      true -> {:ok, render!(module_name, assigns)}
+      false -> {:error, :template_not_loaded}
     end
+  end
 
+  def render!(module_name, assigns) do
     apply(module_name, :render, [assigns])
   end
 
-  def compile(file_path, view_module) when is_binary(file_path) do
-    compile(file_path, module_name(file_path), view_module)
+  def compile(template_string, module_name, view_module) when is_binary(template_string) do
+    EEx.compile_string(template_string)
+    |> compile_ast(module_name, view_module)
   end
 
-  def compile(file_path, module_name, view_module) when is_binary(file_path) and is_atom(module_name) and is_atom(view_module) do
-    EEx.compile_file(file_path)
-    |> quoted_template_module(module_name, view_module)
-    |> Code.compile_quoted()
-
-    :ok
-  end
-
-  def remove_by_file(file_path) when is_binary(file_path) do
-    remove_by_name(module_name(file_path))
-  end
-
-  def remove_by_name(module_name) when is_atom(module_name) do
+  def remove(module_name) when is_atom(module_name) do
     :code.delete(module_name)
     :code.purge(module_name) # this kills all processes using the module (there's also soft_purge)
   end
 
-  def module_name(file_path) do
-    file_path
-    |> get_module_identifier_from_filepath()
-    |> get_qualified_temp_module_name()
+  defp compile_ast(ast, module_name, view_module) when is_atom(module_name) and is_atom(view_module) do
+    ast
+    |> quoted_template_module(module_name, view_module)
+    |> Code.compile_quoted()
+
+    :ok
   end
 
   defp quoted_template_module(quoted_template, module_name, view_module) do
@@ -47,27 +39,5 @@ defmodule DynamicTemplate do
         end
       end
     end
-  end
-
-  defp get_qualified_temp_module_name(module_identifier) do
-    ("Elixir.EExHelpersPriv." <> module_identifier)
-    |> String.to_atom()
-  end
-
-  defp get_module_identifier_from_filepath(path) do
-    # tried to make this regex as: /[^\.]\w/ but it matched every pair of characters (not what I wanted) - reason to have multiple `String.replace`
-
-    path
-    |> Path.rootname()
-    |> Path.basename()
-    |> String.replace(~r/^\w/, fn
-      <<char>> -> <<char + (?A - ?a)>>
-    end)
-    |> String.replace(~r/\.\w/, fn
-      <<?., char>> -> <<char + (?A - ?a)>>
-    end)
-    |> String.replace(~r/\_\w/, fn
-      <<?_, char>> -> <<char + (?A - ?a)>>
-    end)
   end
 end
